871595075160d53f4abe4abe80d102050f2876d2
fumiyadknk <fumiyak74@gmail.com>
2022-10-16 17:19:27 +0900

    バリデーション実装

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Memo;
-use Illuminate\Http\Request;
+use App\Http\Requests\MemoRequest;
 
 class MemoController extends Controller
 {
@@ -18,7 +18,7 @@ class MemoController extends Controller
         return view('memos.create');
     }
 
-    public function store(Request $request)
+    public function store(MemoRequest $request)
     {
         // インスタンスの作成
         $memo = new Memo;
@@ -40,21 +40,24 @@ class MemoController extends Controller
         return view('memos.show', ['memo' => $memo]);
     }
 
-    public function edit($id)
+    public function edit($id) 
     {
         $memo = Memo::find($id);
         return view('memos.edit', ['memo' => $memo]);
     }
 
-    public function update(Request $request, $id)
+    public function update(MemoRequest $request, $id) 
     {
         // ここはidで探して持ってくる以外はstoreと同じ
         $memo = Memo::find($id);
+
         // 値の用意
         $memo->title = $request->title;
         $memo->body = $request->body;
+
         // 保存
         $memo->save();
+
         // 登録したらindexに戻る
         return redirect('/memos');
     }
